5d459efc53e2c70a19fce09a8c0c51b4477a8bd0
serlinecai4935 <tce13145>
2019-01-09 14:20:47 -0500

    h

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -1,3 +1,26 @@
 public class InPlaceSorts {
+    public static void swap(String[] arr,int i, int j){
+        String t = arr[i];
+        arr[i] = arr[j];
+        arr[j]=t;
+    }
 
+    public static void SelectionSort (String[] arr){
+        int curPos = 0;
+        int minPos = 0;
+        int minValue = arr[curPos];
+        while(curPos<arr.length){
+            minPos = curPos;
+            minValue = arr[curPos];
+            for(int i = curPos +1;i<arr.length;i++){
+
+                if(arr[i]<minValue){
+                    minValue = arr[i];
+                    minPos = i;
+                }
+            }
+            swap(arr,minPos,curPos);
+            curPos++;
+        }
+    }
 }
