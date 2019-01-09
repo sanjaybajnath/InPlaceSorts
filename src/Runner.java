e1c179779d666ee0eeecf929ceda7eb73160ef3e
BT_1N3_33 <sbajnath6151@bths.edu>
2019-01-09 14:36:47 -0500

    e

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,22 @@
 public class Runner {
     public static void main(String[] args){
-        System.out.println("a".compareTo("z"));
+        public String[] randomStringArr(int num, int length){
+            String [] arr = new String [num];
+            while (num>0)
+            {
+                int i = 0;
+                String s="";
+                while (i<length)
+                {
+                    char c = (char)((Math.random()*26)+97);
+                    s= s+c;
+                    i++;
+                }
+                num --;
+                arr[num]=s;
+            }
+            return arr;
+        }
 
     }
 }
