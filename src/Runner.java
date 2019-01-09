fec64a2601e05ae71bcf904c4335028eed9b02fe
serlinecai4935 <tce13145>
2019-01-09 14:40:48 -0500

    h

--- src/Runner.java
+++ src/Runner.java
@@ -1,22 +1,22 @@
 public class Runner {
-    public static void main(String[] args){
-        public String[] randomStringArr(int num, int length){
-            String [] arr = new String [num];
-            while (num>0)
-            {
-                int i = 0;
-                String s="";
-                while (i<length)
-                {
-                    char c = (char)((Math.random()*26)+97);
-                    s= s+c;
-                    i++;
-                }
-                num --;
-                arr[num]=s;
+
+    public static String[] randomStringArr(int num, int length){
+        String [] arr = new String [num];
+        while (num>0){
+          int i = 0;
+         String s="";
+         while (i<length){
+            char c = (char)((Math.random()*26)+97);
+            s= s+c;
+            i++;
             }
-            return arr;
-        }
+        num --;
+        arr[num]=s;
+    }
+    return arr;
+    }
+    public static void main(String[] args){
+
 
     }
 }
